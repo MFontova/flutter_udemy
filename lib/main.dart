24ae1284877990b17feda9ab9b5678bbc02c3344
Marc Fontova <mfontovat@gmail.com>
2023-01-02 17:23:10 +0100

    5.46

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:curso_udemy/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        home: Center(
-      child: Text('Hola mundo'),
-    ));
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
